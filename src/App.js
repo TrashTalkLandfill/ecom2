574756d38711e67c8b80127937c8952498982a88
Doug Moore <mantheramparts@gmail.com>
2020-02-04 22:06:40 -0500

    sets up basic components and styles for homepage: homepage, directory, menuitem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import './homepage.styles.scss'
+import HomePage from './pages/homepage/homepage.component';
 
-import HomePage from './homepage.component';
 
 function App() {
   return (
